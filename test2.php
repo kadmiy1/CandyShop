6e13979818c7d5f760def35a01ee027b5e9ca90b
Anton <39156677+AntonSushilov@users.noreply.github.com>
2020-12-02 17:54:11 +0300

    calc

--- test2.php
+++ test2.php
@@ -21,9 +21,7 @@
             <div class="container">
                 <div class="header__inner">
                     <div class="header__logo"><acronym title="Проект студента ФКН Кальченко Дмитрия Алексеевича ">CandyShop</acronym></div>
-
-                     <nav class="nav">
-                    
+                    <nav class="nav">
                         <a class="nav__link" href="registration.php">Регистрация</a>
                         <a class="nav__link" href="authorization.php">Авторизация</a>
                         <a class="nav__link" href="addcandy.php">Добавить сладость</a>
@@ -44,19 +42,15 @@
                     <h1 title="Создание накладной">Создание накладной</h1>
                     <div class="group">
                         <center>Укажите номер накладной</center>
-                        <!-- <label for="">Логин:</label> -->
                         <input type="text"  placeholder="Номер" name="number" id="number" minlength="2">
                     </div>
                     
-                    <div class="group"><center>Укажите дату заказа</center>
-               
-                        <!-- <label for="">Пароль:</label> -->
+                    <div class="group"><center>Укажите дату заказа</center>          
                         <input type="date" placeholder="Дата заказа" name="date" id="date">
                     </div>
                     <div class= "group">  
-
                         <center> Выберите фирму </center>
-                            <select  name= "firm" class="select-chosen">
+                            <select  name= "firm" class="">
                                     <?php 
                                     $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
                                     $sql = "SELECT id, name FROM `firms`" ;
@@ -74,16 +68,39 @@
                         </center>
                     </div>
                     <div>
-                        <table id="tab_1">
+                        <table class="calc" id="tab_1">
                         <tr>
                             <td>Товар</td>
                             <td>Количество</td>
                             <td>Сумма</td>
-                            <td>Действия</td>
                         </tr>
-                        </table>
+                        <?php 
+                            $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
+                            $sql = "SELECT id, name, price FROM `candys`" ;
 
-                        <input class="butadd" type="button" value="+" id="add_1">
+                            $result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
+                            $rows1 = mysqli_num_rows($result);
+                            for ($i = 0 ; $i < $rows1 ; ++$i)
+                            {
+                                echo '<tr  class="t-row">';
+                                $row = mysqli_fetch_row($result);
+                                for ($j = 0 ; $j < 1 ; ++$j){
+                                    echo "<td>$row[1] -  $row[2] р/кг</td>"; 
+                                    echo "<td> <div class='inp-group'>";
+                                    echo "<input type='text' name='quant' value='0' class='quant form-control' data-step='1' data-price=$row[2] data-min='0' />";
+                                    echo "</div></td>";
+                                    echo "<td class='price'>0</td>";
+                                }   
+                                echo "</tr>";
+                            } 
+                            
+                            
+
+                        ?>
+
+
+                        </table>
+                        <p class="text-center result">Итого:<br /><span class="val">0</span> руб.</p>
                     
                         <div class="groupa"> 
                             
@@ -105,85 +122,61 @@
 </html>
 
 <script>
-    
 
-
-    <?php 
-    $link = mysqli_connect($host, $user, $password, $database) or die("Ошибка " . mysqli_error($link));
-    $sql = "SELECT id, name, price FROM `candys`" ;
-
-    $result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
-    $rows1 = mysqli_num_rows($result);
-  
-    ?>
-
-    $(function select(){
-        $('.select-chosen').chosen({
-            width: 200,
-            no_results_text: "Нет такого продукта!",
-            placeholder_text_single: "Выберите продукт",
-            search_contains: true,
-            max_shown_results: 10
+/*калькулятор*/
+    /*Обрабатываем нажатия на кнопки + и - */
+    $('.minpl').click(function() {
+    /*Находим input*/
+        $input = $(this).parent().find('.quant');
+        var qty = Number($input.val());
+        /*На случай, если количество не удалось определить (например, пользователь мог оставить поле пустым)*/
+        if (isNaN(qty)) qty = 0;
+            if ($(this).hasClass('plus')) {
+                if (qty == 0) {
+                    qty = $input.data('min');
+                } else {
+                qty += $input.data('step');
+            }
+        } else {
+            qty -= $input.data('step');
+        }
+        var min = $input.data('min');
+        if (qty >= min) {
+            $input.val(qty).trigger('input');
+        } else {
+            $input.val(0).trigger('input');
+        }
+        /*Передаем функции подсчета, обновления*/
+        updateCalc($input);
+    });
+    /*Обрабатываем ввод с клавиатуры */
+    $('.quant').change(function() {
+        var qty = $(this).val();
+        if (isNaN(qty)) qty = 0;
+        var min = $(this).data('min');
+        var step = $(this).data('step');
+        if (qty > 0) {
+        /*Если вдруг число не кратно шагу, увеличиваем (только увеличение) до кратного*/
+            qty = Math.ceil(qty/step)*step;
+            if (qty < min) {
+                qty = min;
+            }
+            $(this).val(qty).trigger('input');
+        } else {
+            $(this).val(0).trigger('input');
+        }
+        updateCalc($(this));
+    });
+    /*Считаем, обновляем значения*/
+    function updateCalc($input){
+        var qty = Number($input.val());
+        if (isNaN(qty)) qty = 0;
+        $input.parents('.t-row').find('.price').text(qty * $input.data('price')/$input.data('step'));
+        var itog = 0;
+        $input.parents('.calc').find('.price').each(function(){
+            itog += parseInt($(this).text());
         });
-    })
-    var str = '<tr><td><select class="select-chosen"  name="candy[]">'+
-    '<?php 
-        // echo "<option value=0>Выберите сладость</option>";
-    for ($i = 0 ; $i < $rows1 ; ++$i)
-    {
-        $row = mysqli_fetch_row($result);
-        for ($j = 0 ; $j < 1 ; ++$j) echo "<option value=$row[0]>$row[1] -  $row[2] р/кг</option>";   
-        ;
-    } 
-    ?></td>'+
-    '<td><input class="inputadd" type="text" placeholder="Киллограммы" onchange="getmass(this);" name="mass[]" id="massa"></td>'+
-    '<td id = "result">0</td>'+
-    '<td><input type="button" value="-" class="delrow"></td>'+
-    '</select></td></tr>';
-
-    $(function table() {
-    //добавить строку таблицы
-    $('#add_1').click(function(){
-        $('#tab_1').append(str);
-        $('tr')
-        .find('td')
-        .parent()//traversing to 'tr' Element
-        .append('');
-        
-
-        $('.delrow').click(function(){
-            $(this).parent().parent().remove(); //Deleting the Row (tr) Element
-        });
-        $(function select(){
-            $('.select-chosen').chosen({
-                width: 200,
-                no_results_text: "Нет такой сладости!",
-                placeholder_text_single: "Выберите сладость",
-                search_contains: true,
-                max_shown_results: 10
-            });
-        })
-    })
-})
-
-
-    let prise = 0;
-    let mass = 0;
-    let result = 0;
-
-    function getval(sel)
-    {
-        prise = sel.value 
-        $('#pruise').html(prise);
-        
-    }
-
-    function getmass(sel)
-    {
-        mass = sel.value;
-        console.log('масса = ' + mass);
-        result = prise * mass;
-        $('#result').html(result);
+        $input.parents('.calc').next().find('.val').text(itog);
     }
 
 </script>
