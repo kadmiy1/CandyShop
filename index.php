ba446783e7d14ca2aa9f793f3a8123376d83d048
Anton <39156677+AntonSushilov@users.noreply.github.com>
2020-12-02 20:49:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-Hello World!
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>MyStorage</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="header">
+            <div class="container">
+                <div class="header__inner">
+                    <div class="header__logo"><acronym title="Проект студента ФКН Кальченко Дмитрия Алексеевича ">CandyShop</acronym></div>
+
+                    <nav class="nav">
+                    
+                    
+                    <a class="nav__link" href="registration.php">Регистрация</a>
+                        <a class="nav__link" href="authorization.php">Авторизация</a>
+                        <a class="nav__link" href="addcandy.php">Добавить сладость</a>
+                        <a class="nav__link" href="addfirm.php">Добавить фирму</a>
+                        <a class="nav__link" href="candylist.php">Список сладостей</a>
+                        <a class="nav__link" href="firmlist.php">Cписок фирм</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+</body>
+
+</html>
